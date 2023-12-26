ed89dbc3ea1d2bcd257dadccfcb55cb7f8d929ec
marciomartins12 <mmarciom1martins635@gmail.com>
2023-12-26 15:34:26 -0300

    starting the projects page

--- src/Componets/PagePrincipal/PageMain.jsx
+++ src/Componets/PagePrincipal/PageMain.jsx
@@ -1,10 +1,11 @@
 import ApresentacaoAndDescricao from "../ApresentacaoAndDescricao/ApresentacaoAndDescricao";
 import Cabecalho from "../Header/Cabecalho";
+import Style from "./PaginaPrinccipal.module.css";
 
 const PaginaPrincipal = () => {
 
     return (
-        <section>
+        <section className={Style.paginaPrincipalContainer}>
             <Cabecalho />
             <ApresentacaoAndDescricao />
         </section>);

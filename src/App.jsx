1e494ff28d29f6c31c94ae3852800399db80ea4a
marciomartins12 <mmarciom1martins635@gmail.com>
2023-12-27 16:50:01 -0300

    ....

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import PaginaPrincipal from "./Componets/PagePrincipal/PageMain";
 import Projects from "./Componets/Projects/Projects";
 
 function App() {
- 
+ const carregou=()=>{
+  console.log("carregou")
+  
+ }
    return(
-<>
+<section onLoad={carregou()}>
 <PaginaPrincipal/>
 
 <Projects/>
-</>
+</section>
   );
 };
 
--- src/Componets/Projects/Item.jsx
+++ src/Componets/Projects/Item.jsx
@@ -3,20 +3,24 @@ import Style from "./Item.module.css";
 const Item = ({ img, nome, descricao, disponivel, tecnologiausada, link }) => {
     return (
         <div className={Style.container}>
+
             <div className={Style.parte1}>
-                <img src={img} alt={nome} />
-                <p>Disponivel: {disponivel.map((item, indice)=><img key={indice} src={item} alt="icones de dispositivos disponíveis" />)}</p>
+                <img className={Style.img1}src={img} alt={nome} />
+                <div>
+                <p>Disponivel:</p>
+                {disponivel.map((item, indice) => <img key={indice} src={item} alt="icones de dispositivos disponíveis" />)}
+                </div>
 
             </div>
             <div className={Style.parte2}>
-            <h2>{nome}</h2>
-            <p>{descricao}</p>
-            <div>
-                <a href={link}>Abrir</a>
-                {tecnologiausada.map((item, indice)=> <img key={indice}src={item} alt="icone de tecnologias usadas"/> )}
+                <h2>{nome}</h2>
+                <p>{descricao}</p>
+                <div>
+                    <a href={link}>Abrir</a>
+                    {tecnologiausada.map((item, indice) => <img key={indice} src={item} alt="icone de tecnologias usadas" />)}
+                </div>
             </div>
-            </div>
-            <div></div>
+            
         </div>
     );
 };

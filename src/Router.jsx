e0f98fd03b7a81171cc3fc4fe6c69ccb32220c04
marciomartins12 <mmarciom1martins635@gmail.com>
2024-01-04 20:18:13 -0300

    init navbar

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Componet/Navbar";
+
 const Router = ()=>{
     return(
-    <>
-    </>
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element= {<Navbar/>}>
+        <Route path="sobre mim" element= {<div>aaaaaa teste tes</div>}/>
+        </Route>
+
+    </Routes>
+    </BrowserRouter>
     );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Router from './Router'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Router from './Router';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

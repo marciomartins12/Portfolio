4dee0f1bf33d3ddacf3b22861240a2b9cc4343d8
marciomartins12 <mmarciom1martins635@gmail.com>
2024-01-06 18:42:33 -0300

    rotas

--- src/Componet/Navbar/index.jsx
+++ src/Componet/Navbar/index.jsx
@@ -1,17 +1,19 @@
 import { Link, Outlet} from "react-router-dom";
-
+import "./navbar.css";
 const Navbar = () => {
+    const active = window.location.pathname;
+    console.log(active);
     return (
     <>
-    <ul className="nav justify-content-center">
+    <ul className="nav justify-content-center  ">
         <li className="nav-item">
-            <Link to= "" className="nav-link active" aria-current="page" >Início</Link>
+            <Link to= "/" className={`nav-link item-lista ${active === "/"? "ativo": ""}`} aria-current="page" >Início</Link>
         </li>
         <li className="nav-item">
-            <Link to= "" className="nav-link" >Projetos</Link>
+            <Link to= "projetos" className={`nav-link item-lista ${active === "projetos"? "ativo": ""}`}  >Projetos</Link>
         </li>
         <li className="nav-item">
-            <Link to= "" className="nav-link" >Contatos</Link>
+            <Link to= "contatos" className={`nav-link item-lista ${active === "contatos"? "ativo": ""}`}  >Contatos</Link>
         </li>
     </ul>
     <Outlet/>
--- src/Router.jsx
+++ src/Router.jsx
@@ -1,16 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Componet/Navbar";
 
-const Router = ()=>{
-    return(
-    <BrowserRouter>
-    <Routes>
-        <Route path="/" element= {<Navbar/>}>
-        <Route path="sobre mim" element= {<div>aaaaaa teste tes</div>}/>
-        </Route>
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Navbar />}>
+                    <Route index element={<>inicio</>} />
+                    <Route path="projetos" element={<div>projetos</div>} />
+                    <Route path="contatos" element={<div>contatos</div>} />
+                </Route>
 
-    </Routes>
-    </BrowserRouter>
+            </Routes>
+        </BrowserRouter>
     );
 };
 

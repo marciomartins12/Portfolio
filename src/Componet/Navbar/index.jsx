e33b024bb477d06f810aa4010258f8b6c15ef796
marciomartins12 <mmarciom1martins635@gmail.com>
2024-01-06 19:02:43 -0300

    ...

--- src/Componet/Navbar/index.jsx
+++ src/Componet/Navbar/index.jsx
@@ -1,24 +1,31 @@
-import { Link, Outlet} from "react-router-dom";
+import { Link, Outlet, useLocation} from "react-router-dom";
 import "./navbar.css";
+import { useEffect, useState } from "react";
 const Navbar = () => {
-    const active = window.location.pathname;
-    console.log(active);
+    const location = useLocation();
+    const [active, setactive] = useState(location.pathname);
+
+  
+    useEffect(() => {
+      setactive(location.pathname);
+    }, [location.pathname]);
+
     return (
-    <>
+    <main className="main">
     <ul className="nav justify-content-center  ">
         <li className="nav-item">
-            <Link to= "/" className={`nav-link item-lista ${active === "/"? "ativo": ""}`} aria-current="page" >Início</Link>
+            <Link to= "/" className={` item-lista ${active === "/"? 'ativo': ""}`}  >Início</Link>
         </li>
         <li className="nav-item">
-            <Link to= "projetos" className={`nav-link item-lista ${active === "projetos"? "ativo": ""}`}  >Projetos</Link>
+            <Link to="projetos" className={` item-lista ${active === "/projetos"? 'ativo': ""}`}  >Projetos</Link>
         </li>
         <li className="nav-item">
-            <Link to= "contatos" className={`nav-link item-lista ${active === "contatos"? "ativo": ""}`}  >Contatos</Link>
+            <Link to= "contatos" className={` item-lista ${active === "/contatos"? 'ativo': ""}`}  >Contatos</Link>
         </li>
     </ul>
     <Outlet/>
     
-    </>
+    </main>
     );
 };
 

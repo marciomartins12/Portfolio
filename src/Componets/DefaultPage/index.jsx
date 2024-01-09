32569c02aef9b2ff15e81127cba07c5753ea48a1
marciomartins12 <mmarciom1martins635@gmail.com>
2024-01-09 14:29:01 -0300

    home page just missing  style

--- src/Componets/DefaultPage/index.jsx
+++ src/Componets/DefaultPage/index.jsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../Navbar";
 import Panel from "../Panel";
-
+import Style from "./defaultpage.module.css";
 const DefaultPage = () => {
     return (
-       <section>
+       <section className={Style.containerfull}>
         <NavBar/>
         <Panel/>
         <Outlet/>
--- src/Componets/Home/index.jsx
+++ src/Componets/Home/index.jsx
@@ -5,8 +5,25 @@ const Home = () => {
         <section className={Style.container}>
             <div>
                 <h2>I am a Developer web.</h2>
-                <p>I build things for web.</p>
-                <Link className={Style.btn}to="/works">My projects</Link>
+                <p>I build <span className={Style.span}>things for web.</span></p>
+                <div>
+
+                    <span>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero impedit quod expedita!
+                    </span>
+                </div>
+
+                <div className={Style.containerbtn}>
+                    <div className={Style.center}>
+                        <button className={Style.btn}>
+                            <svg width="180px" height="60px" viewBox="0 0 180 60" className={Style.border}>
+                                <polyline points="179,1 179,59 1,59 1,1 179,1" className={Style.bgline} />
+                                <polyline points="179,1 179,59 1,59 1,1 179,1" className={Style.hlline}/>
+                            </svg>
+                            <span>HOVER ME</span>
+                        </button>
+                    </div>
+                </div>
             </div>
 
             <img src="/assets/img/homepage.svg" alt="homepage" />

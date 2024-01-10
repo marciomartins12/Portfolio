41776defde414ef4800dd5b0dee1ae19a394d879
marciomartins12 <mmarciom1martins635@gmail.com>
2024-01-10 14:07:33 -0300

    .;

--- src/Componets/About/index.jsx
+++ src/Componets/About/index.jsx
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from 'react';
 import Style from './aboutme.module.css';
 
 const About = () => {
+    const [click, setclick] = useState("skills")
+    const handleclick = (text) => {
+        setclick(text)
+    };
     const [count0to1, setCount0to1] = useState(0);
     const [count0to12, setCount0to12] = useState(0);
 
@@ -38,26 +42,57 @@ const About = () => {
             </div>
             <div>
                 <nav className={Style.nav}>
-                    <button>Skills</button>
-                    <button>Credentials</button>
-                    <button>Expereinces</button>
+                    <button className={click === "skills" ? Style.ativo : ""} onClick={() => handleclick("skills")}>Skills</button>
+                    <button className={click === "credentials" ? Style.ativo : ""} onClick={() => handleclick("credentials")}>Credentials</button>
+                    <button className={click === "experiences" ? Style.ativo : ""} onClick={() => handleclick("experiences")}>Expereinces</button>
                 </nav>
-                <div className={Style.divconteudo}>
-                    <div>Developer</div>
-                    -
-                    <div className={Style.divimgicons}>
-                        <div><img src="/assets/img/react.png" alt="icon" /></div>
-                        <div><img src="/assets/img/js.png" alt="icon" /></div>
-                        <div><img src="/assets/img/html.png" alt="icon" /></div>
-                        <div><img src="/assets/img/css.png" alt="icon" /></div>
-                        <div><img src="/assets/img/php.png" alt="icon" /></div>
+                {click === "skills" ?
+                    <div>
+                        <div className={Style.divconteudo}>
+                            <div>Developer</div>
+                            -
+                            <div className={Style.divimgicons}>
+                                <div><img src="/assets/img/react.png" alt="icon" /></div>
+                                <div><img src="/assets/img/js.png" alt="icon" /></div>
+                                <div><img src="/assets/img/html.png" alt="icon" /></div>
+                                <div><img src="/assets/img/css.png" alt="icon" /></div>
+                                <div><img src="/assets/img/php.png" alt="icon" /></div>
+                            </div>
+                        </div>
+                    </div>
+                    : <></>}
+
+
+                {click === "credentials" ? <div>
+                    <div className={Style.divconteudo}>
+                        <p>Undergraduate</p>
+                        -
+                        <h3>Computer Science</h3>
+                    </div>
+                    <div className={Style.divconteudo}>
+                        <p>Completed courses</p>
+                        -
+                        <a target="_blank" href="https://cursos.alura.com.br/user/marciom1martins635/fullCertificate/ec4a9e4212cf8ec94ce64ff1cee49545">click me <img src="/assets/img/doc.png" alt="doc img" /></a>
                     </div>
                 </div>
+                    : <></>}
 
-
-                <div></div>
-                <div></div>
-
+                {click === "experiences" ?
+                    <div>
+                        <div className={Style.divconteudo}>
+                            <p>Projects</p>
+                            -
+                            <p>
+                                My experience so far is centered on personal projects.
+                            </p>
+                        </div>
+                        <div className={Style.divconteudo}>
+                            <p>Explore</p>
+                            -
+                            <a href="github.com/marciomartins12">click me<img src="/assets/img/github.png" alt="github img" /></a>
+                        </div>
+                    </div>
+                    : <></>}
 
 
             </div>

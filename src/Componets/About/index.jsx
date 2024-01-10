ccb03278cd16334f38263079f3d1da771f42ba1b
marciomartins12 <mmarciom1martins635@gmail.com>
2024-01-10 12:48:30 -0300

    init about

--- src/Componets/About/index.jsx
+++ src/Componets/About/index.jsx
@@ -1,10 +1,39 @@
-import Style from "./aboutme.module.css";
+import React, { useState, useEffect } from 'react';
+import Style from './aboutme.module.css';
 
 const About = () => {
-    return(
-        <section>
+    const [count0to1, setCount0to1] = useState(0);
+    const [count0to12, setCount0to12] = useState(0);
 
+    useEffect(() => {
+        const interval1 = setInterval(() => {
+            setCount0to1((prevCount) => (prevCount < 1 ? prevCount + 1 : 1));
+        }, 1000);
 
+        const interval2 = setInterval(() => {
+            setCount0to12((prevCount) => (prevCount < 12 ? prevCount + 1 : 12));
+        }, 500);
+
+        return () => {
+            clearInterval(interval1);
+            clearInterval(interval2);
+        };
+    }, []);
+
+    return (
+        <section className={Style.container}>
+            <div>
+                <p></p>
+                <div>
+                    <h3>{count0to1}</h3>
+                    <p>year of experiences</p>
+                </div>
+                <div>
+                    <h3>{Math.round(count0to12)}</h3>
+                    <p>Finished projects</p>
+                </div>
+            </div>
+            <div></div>
         </section>
     );
 };

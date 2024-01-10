737c61537d7c825c9853d134345b22027945d5f2
marciomartins12 <mmarciom1martins635@gmail.com>
2024-01-10 18:38:42 -0300

    ;,

--- src/Componets/DefaultPage/index.jsx
+++ src/Componets/DefaultPage/index.jsx
@@ -4,11 +4,13 @@ import Panel from "../Panel";
 import Style from "./defaultpage.module.css";
 const DefaultPage = () => {
     return (
-       <section className={Style.containerfull}>
-        <NavBar/>
-        <Panel/>
-        <Outlet/>
-       </section>
+        <section className={Style.containerfull}>
+            <NavBar />
+            <Panel />
+            <div className={Style.content}>
+                <Outlet />
+            </div>
+        </section>
     );
 };
 export default DefaultPage;

d1fe970269a5ce09a39db024d85bdb921c81a90b
marciomartins12 <mmarciom1martins635@gmail.com>
2024-01-10 12:27:47 -0300

    page projects

--- src/Componets/Projects/index.jsx
+++ src/Componets/Projects/index.jsx
@@ -8,33 +8,45 @@ const Projects = () => {
                 <h2>My projects
                     <span>.</span>
                 </h2>
-                <p>In my portfolio, I exclusively highlight personal projects. These creations reflect not only my technical skills, but also the passion I have for constantly learning and evolving.</p>
+                <p className={Style.description}>I exclusively <span>highlight personal</span> projects. These creations reflect not only my technical skills, but also the passion I have for constantly learning and evolving.</p>
 
             </div>
             <div>
                 <div className={Style.divimg}>
                     <div className={Style.containerImgs}>
                         <div className={Style.div}>
-                            <img src="/assets/imgProjects/alurabooks.png" alt="" />
+                            <img src="/assets/imgProjects/alurabooks.png" alt="alura books img" />
                             <div className={Style.title}>
                                 <a href="#" target="_blank">Live project</a>
                             </div>
                         </div>
                         <div className={Style.div}>
-                            <img src="/assets/imgProjects/estacaosuplemento.png" alt="" />
+                            <img src="/assets/imgProjects/estacaosuplemento.png" alt="estação suplementos img" />
                             <div className={Style.title}>
                                 <a href="#" target="_blank">Live project</a>
                             </div>
                         </div>
                     </div>
                     <div className={Style.containerImgs}>
-                        <img src="/assets/imgProjects/test.jpg" alt="" />
-                        <img src="/assets/imgProjects/test.jpg" alt="" />
-
+                        <div className={Style.div}>
+                            <img src="/assets/imgProjects/fraudebank.png" alt="fraude bank img" />
+                            <div className={Style.title}>
+                                <a href="#" target="_blank">Live project</a>
+                            </div>
+                        </div>
+                        <div className={Style.div}>
+                            <img src="/assets/imgProjects/chat.png" alt="chat img" />
+                            <div className={Style.title}>
+                                <a href="#" target="_blank">Live project</a>
+                            </div>
+                        </div>
 
                     </div>
-                    <button>-{">"}</button>
-                    <button>{"<"}-</button>
+                    <div className={Style.buttondiv}>
+                        <button className={Style.button}><img src="/assets/img/buttonenable.png" alt="button enable" /></button>
+                        <button className={Style.button}><img src="/assets/img/button.png" alt="button enable" /></button>
+                    </div>
+
                 </div>
             </div>
 

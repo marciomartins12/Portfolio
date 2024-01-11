1b0193e55e77b1734ddc9cbd5bbc9785d036d9d9
marciomartins12 <mmarciom1martins635@gmail.com>
2024-01-10 22:26:19 -0300

    ./

--- src/Componets/Transition/index.jsx
+++ src/Componets/Transition/index.jsx
@@ -1,27 +1,37 @@
 
 import Style from "./transition.module.css";
-import {motion} from "framer-motion"
+import { motion } from "framer-motion"
+
 const Transition = () => {
+
     const transitionVariants = {
         initial: {
-            x: "100%",
-            width: "100%"
+            x: '100%',
+            width: '100%',
         },
         animate: {
-            x: "0%",
-            width: "0%"
+            x: '0%',
+            width: '0%',
         },
         exit: {
-            x: ["0%", "100%"],
-            width: ["0%", "100%"]
+            x: ['0%', '100%'],
+            width: ['0%', '100%'],
         }
-    }
+    };
     return (
         <>
             <motion.div className={Style.divtransition}
-                variants={transitionVariants} initial="initial" animate="animate" exit="exit" transition={{ delay: 0.1, duration: 0.7, ease: "easeInOut" }}
-            >1</motion.div>
-            
+                variants={transitionVariants} initial='initial' animate='animate' exit='exit' transition={{ delay: 0.1, duration: 0.9, ease: "easeInOut" }}
+            ></motion.div>
+
+            <motion.div className={Style.divtransition}
+                variants={transitionVariants} initial='initial' animate='animate' exit='exit' transition={{ delay: 0.4, duration: 0.8, ease: "easeInOut" }}
+            ></motion.div>
+
+            <motion.div className={Style.divtransition}
+                variants={transitionVariants} initial='initial' animate='animate' exit='exit' transition={{ delay: 0.2, duration: 0.6, ease: "easeInOut" }}
+            ></motion.div>
+
         </>
     );
 };

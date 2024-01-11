c1f51cd52ad7e5c3832ab8cefb08c87eda4dbe9b
marciomartins12 <mmarciom1martins635@gmail.com>
2024-01-11 01:21:30 -0300

    page contacts

--- src/Componets/Contacts/index.jsx
+++ src/Componets/Contacts/index.jsx
@@ -5,23 +5,24 @@ const Contacts = () => {
         <section className={Style.cont}>
             <h2>Talk t<span>o me</span></h2>
             <div className={Style.containercontacts}>
-                <form>
+                <form className={Style.containerpart1}>
                     <div className={Style.containerinputs}>
                         <input type="text" placeholder="Name" />
                         <textarea className={Style.textarea} placeholder="Message" />
                     </div>
                     <button>Enviar</button>
                 </form>
-                <div>
+                <div className={Style.containerpart2}>
                     <h3>Social networks:</h3>
-                    <div>
+                    <div className={Style.divnetworks}>
                         <img src="/assets/img/instagram.png" alt="instagram logo" />
                         <img src="/assets/img/facebook.png" alt="facebook logo" />
                         <img src="/assets/img/whatsapp.png" alt="whatsapp web" />
                         <img src="/assets/img/github.png" alt="github logo" />
                     </div>
-                    <h3>Contacts:</h3>
+                    <h4>Contacts:</h4>
                     <p>mmarciom1martins635@gmail.com</p>
+                    <p>marciom1martins635@gmail.com</p>
                     <p>55 98981024732</p>
 
                 </div>

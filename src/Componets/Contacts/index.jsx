61ab0e380a61b861f1be389a45fc09d66b4542d4
marciomartins12 <mmarciom1martins635@gmail.com>
2024-01-11 01:07:03 -0300

    .>.

--- src/Componets/Contacts/index.jsx
+++ src/Componets/Contacts/index.jsx
@@ -1,9 +1,31 @@
 import Style from "./contacts.module.css";
 
-const Contacts = ()=>{
+const Contacts = () => {
     return (
-        <section>
+        <section className={Style.cont}>
+            <h2>Talk t<span>o me</span></h2>
+            <div className={Style.containercontacts}>
+                <form>
+                    <div className={Style.containerinputs}>
+                        <input type="text" placeholder="Name" />
+                        <textarea className={Style.textarea} placeholder="Message" />
+                    </div>
+                    <button>Enviar</button>
+                </form>
+                <div>
+                    <h3>Social networks:</h3>
+                    <div>
+                        <img src="/assets/img/instagram.png" alt="instagram logo" />
+                        <img src="/assets/img/facebook.png" alt="facebook logo" />
+                        <img src="/assets/img/whatsapp.png" alt="whatsapp web" />
+                        <img src="/assets/img/github.png" alt="github logo" />
+                    </div>
+                    <h3>Contacts:</h3>
+                    <p>mmarciom1martins635@gmail.com</p>
+                    <p>55 98981024732</p>
 
+                </div>
+            </div>
         </section>
     );
 };

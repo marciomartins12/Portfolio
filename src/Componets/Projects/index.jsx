08d3f1d33fb7e31f540b36e73e3f4c93e7078b84
marciomartins12 <mmarciom1martins635@gmail.com>
2024-01-14 10:49:23 -0300

    ..link sites

--- src/Componets/Projects/index.jsx
+++ src/Componets/Projects/index.jsx
@@ -16,13 +16,13 @@ const Projects = () => {
                         <div className={Style.div}>
                             <img src="/assets/imgProjects/alurabooks.png" alt="alura books img" />
                             <div className={Style.title}>
-                                <a href="#" target="_blank">Live project</a>
+                                <a href="https://relaxed-belekoy-9b92a3.netlify.app/" target="_blank">Live project</a>
                             </div>
                         </div>
                         <div className={Style.div}>
                             <img src="/assets/imgProjects/estacaosuplemento.png" alt="estação suplementos img" />
                             <div className={Style.title}>
-                                <a href="#" target="_blank">Live project</a>
+                                <a href="https://kaleidoscopic-toffee-4ff3b0.netlify.app/" target="_blank">Live project</a>
                             </div>
                         </div>
                     </div>
@@ -30,13 +30,13 @@ const Projects = () => {
                         <div className={Style.div}>
                             <img src="/assets/imgProjects/fraudebank.png" alt="fraude bank img" />
                             <div className={Style.title}>
-                                <a href="#" target="_blank">Live project</a>
+                                <a href="https://super-kringle-75858c.netlify.app/" target="_blank">Live project</a>
                             </div>
                         </div>
                         <div className={Style.div}>
                             <img src="/assets/imgProjects/chat.png" alt="chat img" />
                             <div className={Style.title}>
-                                <a href="#" target="_blank">Live project</a>
+                                <a href="https://moonlit-pithivier-742d7d.netlify.app/" target="_blank">Live project</a>
                             </div>
                         </div>
 
